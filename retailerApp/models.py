6cdc3971e6ca84c89a9b6248981cb754d9a574a4
mehediarrahman <mehediarrahman126@gmail.com>
2025-11-13 18:45:38 +0600

    simple changes into retailerApp

--- retailerApp/models.py
+++ retailerApp/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from accountsApp.models import User
 
 class RetailerProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    shop_name = models.CharField(max_length=100)
-    trade_license = models.CharField(max_length=50)
-    location = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    shop_name = models.CharField(max_length=100,null=True)
+    trade_license = models.CharField(max_length=50,null=True)
+    location = models.CharField(max_length=100,null=True)
     
     def str(self):
         return f"{self.shop_name} - {self.user.username}"
 
 class RetailerStock(models.Model):
-    retailer = models.ForeignKey(RetailerProfile, on_delete=models.CASCADE)
-    stock_out = models.ForeignKey('warehouseapp.StockOut', on_delete=models.CASCADE)
-    selling_price_per_kg = models.DecimalField(max_digits=8, decimal_places=2)
-    is_approved = models.BooleanField(default=False)
+    retailer = models.ForeignKey(RetailerProfile, on_delete=models.CASCADE,null=True)
+    stock_out = models.ForeignKey('warehouseapp.StockOut', on_delete=models.CASCADE,null=True)
+    selling_price_per_kg = models.DecimalField(max_digits=8, decimal_places=2,null=True)
+    is_approved = models.BooleanField(default=False,null=True)
     
     def str(self):
         return f"RetailerStock - {self.retailer.shop_name}"
