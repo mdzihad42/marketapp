bd05140b23c1fef262a1747cc45f3d0ba4a57186
mehediarrahman <mehediarrahman126@gmail.com>
2025-11-13 23:53:05 +0600

    retailler App Created

--- retailerApp/models.py
+++ retailerApp/models.py
@@ -1,20 +1,49 @@
 from django.db import models
-from accountsApp.models import User
+from accountsApp.models import *
+from farmarApp.models import *
 
 class RetailerProfile(models.Model):
+    Status=[
+        ('Active','Active'),    
+        ('In Active','In Active'),    
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
     shop_name = models.CharField(max_length=100,null=True)
-    trade_license = models.CharField(max_length=50,null=True)
-    location = models.CharField(max_length=100,null=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE,null=True)
+    field_agent = models.ForeignKey(FieldAgent, on_delete=models.CASCADE,null=True)
+    phone  = models.CharField(max_length=150, null=True)
+    status = models.CharField(choices=Status, max_length=50, null=True)
     
     def str(self):
         return f"{self.shop_name} - {self.user.username}"
+    
+class Product(models.Model):
+    Unit=[
+        ('Kg','Kg'),    
+        ('Liter','Liter'),    
+        ('etc','etc'),    
+    ]
+    Category=[
+        ('Food','Food'),    
+        ('Vegetable','Vegetable'),    
+        ('etc','etc'),    
+    ]
+    crop = models.ForeignKey(Crop, on_delete=models.CASCADE,null=True)
+    name = models.CharField( max_length=250, null=True)
+    unit = models.CharField(choices=Unit, max_length=50,null=True)
+    catagory = models.CharField(choices=Category, max_length=50,null=True)
+    created_at = models.DateField(auto_now_add=True,null=True)
 
-class RetailerStock(models.Model):
+
+class RetailerProductPrice(models.Model):
     retailer = models.ForeignKey(RetailerProfile, on_delete=models.CASCADE,null=True)
-    stock_out = models.ForeignKey('warehouseapp.StockOut', on_delete=models.CASCADE,null=True)
-    selling_price_per_kg = models.DecimalField(max_digits=8, decimal_places=2,null=True)
-    is_approved = models.BooleanField(default=False,null=True)
+    crop = models.ForeignKey(Crop, on_delete=models.CASCADE,null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,null=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE,null=True)
+    field_agent = models.ForeignKey(FieldAgent, on_delete=models.CASCADE,null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2,null=True)
+    created_at = models.DateField(auto_now_add=True,null=True)
+    verified = models.BooleanField(default=False,null=True)
     
     def str(self):
-        return f"RetailerStock - {self.retailer.shop_name}"
+        return f"Retailer: - {self.retailer.shop_name}"
